788e2629c0c4e48dadf86631ffb1a599235704ff
pablogon99 <60425094+pablogon99@users.noreply.github.com>
2021-04-18 12:09:34 +0200

    commit a medias de la creacion de la base de datos

---
+++ aplicaciongym/database/seeders/UsuarioSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    
+    }
+}
